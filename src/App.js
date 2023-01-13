5920eac1ec25e739c804c0cfdec7ccb34108974e
Owen McComas <owen@mccomas.me>
2023-01-13 14:39:57 -0700

    WIP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <Router>
+      <div className="App">
+        <h1>Lambda Eats</h1>
+        <p>Food store</p>
+        <div>
+            <Route path='/'>
+              <Link to='/'>
+                <button>click</button>
+              </Link>
+              <Link to='/other-click'>
+                <button>other click</button>
+              </Link>
+            </Route>
+        </div>
+      </div>
+    </Router>
   );
 };
 export default App;
+
