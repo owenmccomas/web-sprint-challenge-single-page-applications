61a8306333d5f32e13401adb2587bf8a52ce4804
Owen McComas <owen@mccomas.me>
2023-01-14 01:17:18 -0700

    Work in Progress

--- src/App.js
+++ src/App.js
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Form from "./Form";
+import * as yup from 'yup'
 
 const App = () => {
+
+  const initialZaData = {
+      name: '',
+      size: '',
+      pepperoni: false,
+      cheese: false,
+      sauce: false,
+      crust: false,
+      special: ''
+  }
+  const [errors, setErrors] = useState({
+      name: '',
+      size: '',
+      pepperoni: '',
+      cheese: '',
+      sauce: '',
+      crust: '',
+      special: ''
+  })
+
+  const handleChange = (evt) => {
+    const { name, value } = evt.target
+    validate(name, value)
+    setZaData({...zaData, [name]: value})
+  }
+
+  const [zaData, setZaData] = useState(initialZaData)
+
+  const validate = (name, value) => {
+    yup.reach(formSchema, name)
+       .validate(value)
+       .then(vaild => setErrors({...errors, [name]: ''}))
+       .catch(err => {
+        setErrors({...errors, [name]: err.errors[0]})
+       })
+       console.log('validate called')
+  }
+
+
+  const formSchema = yup.object().shape({
+    name: yup
+      .string()
+      .required(2, 'name must be at least 2 characters'),
+    size: yup
+      .string()
+      .required('must pick a size for your pizza'),
+    pepperoni: yup
+      .boolean(),
+    cheese: yup
+      .boolean(),
+    sauce: yup
+      .boolean(),
+    crust: yup
+      .boolean(),
+    special: yup
+      .string()
+  })
+ 
+
+
   return (
     <Router>
       <div className="App">
@@ -10,11 +72,17 @@ const App = () => {
         <div>
             <Route path='/'>
               <Link to='/'>
-                <button>click</button>
+                <button>home</button>
               </Link>
-              <Link to='/other-click'>
-                <button>other click</button>
+              <Link to='/pizza'>
+                <button>pizza</button>
               </Link>
+                <Form
+                //  submit={submit}
+                 change={handleChange} 
+                 zaData={zaData}
+
+                />
             </Route>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<StrictMode><BrowserRouter><App /></BrowserRouter></StrictMode>, document.getElementById("root"));
